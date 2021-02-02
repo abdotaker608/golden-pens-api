0178d6d9b30ba114894a2f86b684682531b295b1
EL- Dababa <abdotaker608@gmail.com>
2021-02-02 17:37:09 +0200

    'Initial'

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import User, Author
+from django.db.models import Count
 
 
 class AuthorInline(admin.StackedInline):
@@ -12,7 +13,12 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name', 'pk', 'email', 'author__nickname']
     list_display = ['pk', 'get_full_name', 'get_author_nickname', 'get_followers_count']
     inlines = [AuthorInline]
-    ordering = ('author__followers_count', )
+    ordering = ('-top', )
+
+    def get_queryset(self, request):
+        qs = super(UserAdmin, self).get_queryset(request)
+        qs = qs.annotate(top=Count('author__followers'))
+        return qs
 
     def get_full_name(self, instance):
         return f'{instance.first_name} {instance.last_name}'
